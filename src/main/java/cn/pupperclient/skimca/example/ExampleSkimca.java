b92e3c88a83f5cb6a70107bbc5eab92fb55c3b4b
oneachina <ruiruixi@hotmail.com>
2026-01-28 22:25:57 +0800

    fix(ExampleSkimca): ????

--- src/main/java/cn/pupperclient/skimca/example/ExampleSkimca.java
+++ src/main/java/cn/pupperclient/skimca/example/ExampleSkimca.java
@@ -3,6 +3,7 @@ package cn.pupperclient.skimca.example;
 import cn.pupperclient.skimca.Skimca;
 import cn.pupperclient.skimca.SkimcaClient;
 import cn.pupperclient.skimca.SkimcaLogger;
+import cn.pupperclient.skimca.context.SkiaContext;
 import cn.pupperclient.skimca.event.EventTarget;
 import cn.pupperclient.skimca.event.RenderSkiaEvent;
 import cn.pupperclient.skimca.font.FontHelper;
@@ -13,7 +14,7 @@ import java.awt.*;
 public class ExampleSkimca {
     @EventTarget(priority = EventTarget.Priority.HIGH)
     public void onInGameRender(RenderSkiaEvent event) {
-        SkimcaLogger.info("ExampleSkimca", "render");
+        // SkimcaLogger.info("ExampleSkimca", "render");
         MinecraftClient client = MinecraftClient.getInstance();
         String versionText = "Skimca v" + SkimcaClient.Version;
 
@@ -22,25 +23,24 @@ public class ExampleSkimca {
                 8,
                 "/assets/skimca/fonts/Inter-Regular-CJKsc.ttf"
         );
-
-        // 在右上角显示
         var textBounds = Skimca.getTextBounds(versionText, font);
         float x = client.getWindow().getWidth() / 2f;
         float y = client.getWindow().getHeight() / 2f;
 
-        Skimca.translate(x, y);
+        SkiaContext.draw((skiaContext) -> {
 
-        Skimca.save();
+            Skimca.save();
 
-        Skimca.drawText(
-                versionText,
-                x,
-                y,
-                new Color(255, 255, 255, 180),
-                font
-        );
+            Skimca.drawText(
+                    versionText,
+                    x,
+                    y,
+                    new Color(255, 255, 255, 180),
+                    font
+            );
 
-        Skimca.restore();
-        SkimcaLogger.info("ExampleSkimca", "render/restore");
+            Skimca.restore();
+        });
+        // SkimcaLogger.info("ExampleSkimca", "render/restore");
     }
 }
